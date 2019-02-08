13e4d5d798a63e61e9190358c8ca90c55eddc361
Anton <anton.stange@malmberg.nl>
2019-02-08 17:18:00 +0100

    Updated with commandline arguments, thanks to Martin

--- RemoveColumnsFromCsvFile.py
+++ RemoveColumnsFromCsvFile.py
@@ -6,22 +6,21 @@ Created on 21 apr. 2018
 import csv, sys
 import datetime
 from pathlib import Path
+import sys
 
-
-PATH = 'D:\\temp\\test\\'
-FILENAME_IN = 'file.csv'
-REMOVE_COLUMNS = ['columNameA','columNameB']
+REMOVE_COLUMNS = ['columnNameA','columnNameB']
+filename = sys.argv[-1]
 
 
 def main():
-    csvFileIn = PATH + FILENAME_IN
+    csvFileIn = filename
     writeToLog('Reading csv file = %s' %csvFileIn)
-    
+
     csvLines = csvFileToList(csvFileIn, 'r')
     headerLine = csvLines[0]
-    
+
     writeToLog('Determine what columns should be removed from the csv file')
-    
+
     indexesToRemove = []
     for i in range(len(headerLine)):
         removeIt = False
@@ -30,30 +29,30 @@ def main():
                 removeIt = True
         if removeIt:
             indexesToRemove.append(i)
-    
+
     indexesToRemove.sort(reverse = True)
-            
+
     for index in indexesToRemove:
         writeToLog('Based on the header of the CSV file column %s will be removed' %index)
-        
+
     newCsvLines = []
     for line in csvLines:
         for index in indexesToRemove:
             line.pop(index)
         newCsvLines.append(line)
-    
-    filenameOut = FILENAME_IN.split('.')[0] + '_OUT.' + FILENAME_IN.split('.')[1]
-    csvFileOut = PATH + filenameOut
+
+    filenameOut = filename.split('.')[0] + '_OUT.' + filename.split('.')[1]
+    csvFileOut = filenameOut
     writeToLog('Writing to file %s' %csvFileOut)
-    
-    with open(csvFileOut, 'w', newline='\n') as csvfile:    
-        csvWriter = csv.writer(csvfile, delimiter=',',quotechar='"',quoting=csv.QUOTE_ALL, lineterminator="\n") 
+
+    with open(csvFileOut, 'w', newline='\n') as csvfile:
+        csvWriter = csv.writer(csvfile, delimiter=',',quotechar='"',quoting=csv.QUOTE_ALL, lineterminator="\n")
         for line in newCsvLines:
             csvWriter.writerow(line)
-    
+
     writeToLog('Done!')
-             
-        
+
+
 def csvFileToList(fileName, readType):
     csvList = []
     file = Path(fileName)
@@ -80,7 +79,7 @@ def writeToLog(logValue):
 def writeLineToFile(fileNameOut, appendLine):
     f = open(fileNameOut,'a')
     f.write(appendLine+'\n')
-    
-    
+
+
 if __name__ == '__main__':
     main()
